6e96e10fa491557d96a567b1155b4d2d2a71ea1d
tannisthamaiti <37048897+tannisthamaiti@users.noreply.github.com>
2024-03-23 01:47:12 -0400

    Add files via upload

--- movies_recs.py
+++ movies_recs.py
@@ -1,11 +1,11 @@
 import pymongo
 import requests
 
-client = pymongo.MongoClient("mongodb+srv://mtannistha:<password>@cluster0.xbgttui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = pymongo.MongoClient("mongodb+srv://mtannistha:tacumqv6IMOJsI2l@cluster0.xbgttui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db= client.sample_mflix
 collection = db.movies
 
-hf_token = "<hf_token>"
+hf_token = "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"
 embedding_url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
 
 
@@ -21,8 +21,23 @@ def generate_embedding(text: str) -> list[float]:
 
   return response.json()
 
-query = "imaginary characters from outer space at war"
+query = "Dinosaurus back to life"
+### plot_embedding field generated for first 50 entries
+# for doc in collection.find({'plot':{"$exists":True}}).limit(100):
+#     doc['plot_embedding_hf']= generate_embedding(doc['plot']) #introduce a new field with plot_embedding and replace the old doc
+#     collection.replace_one({'_id': doc['_id']}, doc)
+### find documents in the collection using vectorSerch field where the plot_embedding_hf field is semantically similar to the 
+#provided query, PlotSemanticSearch is the name of the search, numCandidated:how many candidates matches internally before returning 
+#final result, top 4 matches
+results = collection.aggregate([
+  {"$vectorSearch": {
+    "queryVector": generate_embedding(query),
+    "path": "plot_embedding_hf",
+    "numCandidates": 100,
+    "limit": 4,
+    "index": "PlotSemanticSearch",
+      }}
+])
 
-for doc in collection.find({'plot':{"$exists":True}}).limit(50):
-    doc['plot_embedding_hf']= generate_embedding(doc['plot']) #introduce a new field with plot_embedding and replace the old doc
-    collection.replace_one({'_id': doc['_id']}, doc)
+for document in results:
+    print(f'Movie Name: {document["title"]},\nMovie Plot: {document["plot"]}\n')
